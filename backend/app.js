bd98d1d29475522e193b19b143041c2c8c7b953f
Maria Gromyko <mazvereva91@mail.ru>
2022-11-27 19:08:43 +0300

    fix: CORS

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import process from 'process';
-import cors from 'cors';
+// import cors from 'cors';
 import { constants } from 'http2';
 import { errors } from 'celebrate';
 import { userRoutes } from './routes/users.js';
@@ -26,8 +26,50 @@ mongoose.connect('mongodb://localhost:27017/mestodb'); // подключаемс
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
 
-app.use(cors());
-app.options('*', cors());
+// **** nmp КОРС 
+// app.use(cors());
+
+
+// **** Простой КОРС запрос (GET, POST, HEAD запросы). Разрешаем доступ с определенных источников
+const allowedCors = [
+'mariagrom.mesto.nomoredomains.club'
+];
+
+app.use(function(req, res, next) {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых 
+  if (allowedCors.includes(origin)) {
+    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  next();
+});
+
+// **** Сложный КОРС запрос (PUT-, PATCH- и DELETE- запросы). Предварительные запросы
+const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+
+// Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE"; 
+
+// Если это предварительный запрос, добавляем нужные заголовки
+if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию) 
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+}
+
+// сохраняем список заголовков исходного запроса 
+const requestHeaders = req.headers['access-control-request-headers']; 
+if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы с этими заголовками
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    return res.end();
+}
+
+
+
+
+
 
 // Подключаем логгер запросов
 app.use(requestLogger);
